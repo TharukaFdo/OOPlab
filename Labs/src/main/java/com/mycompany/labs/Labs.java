fedbf8d077048476abde4487b426c386381846d7
Sahan Tharuka Fernando <sahantharukaedu@gmail.com>
2026-04-10 13:21:41 +0530

    initial

---
+++ Labs/src/main/java/com/mycompany/labs/Labs.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.labs;
+
+/**
+ *
+ * @author Tharu
+ */
+public class Labs {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
